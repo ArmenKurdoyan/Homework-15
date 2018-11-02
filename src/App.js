174c9e4f1c99a9c71bfed6ce965d1fffbda62139
Armen Kurdoyan <knan2599@gmail.com>
2018-11-02 18:29:43 +0400

    fix fetching data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Users from './components/Users';
+import Albums from './components/Albums';
+import Photos from './components/Photos';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          
+          <div className="users-albums">
+          
+            <div className="users">
+              <Link to="/users"><div>Users</div></Link>
+              <Route path="/users" component={Users} />
+            </div>
+
+            <div className="albums">
+              <Route path="/users/:userId" component={Albums} />
+            </div>
+          </div>
+
+          <div className="photos">
+            <Route path="/albums/:albumId" component={Photos} />
+          </div>
+
+        </div>
+      </Router>
     );
   }
 }
